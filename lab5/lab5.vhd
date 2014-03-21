9fad708c6fdfe89e2aad68a6ddc3c3276b99accd
g16-sw <mcw918@hotmail.com>
2014-03-20 21:55:14 -0700

    Revert "Revert "Revert "Revert "fixed clear screen""""
    This reverts commit 5153cba9777f7410d2d9fe35b9329711b1bc6932.

--- lab5/lab5.vhd
+++ lab5/lab5.vhd
@@ -84,129 +84,131 @@ begin
 		
 	end process;
 	
-	-- process for the paddles:
-	process(CLOCK_SLOW) 
-	
-		variable CURRENT_STATE : unsigned(3 DOWNTO 0) := "0000";
-	
-		variable P1_GOAL	: unsigned(6 downto 0);
-		variable P1G_X		: unsigned(7 downto 0);
-		
-		variable P1_FORW	: unsigned(6 downto 0);
-		variable P1F_X		: unsigned(7 downto 0);
-		
-	begin 
-	
-		if (rising_edge(CLOCK_SLOW)) then
-		
-		if (key(3) = '0') then
-			CURRENT_STATE := "0000";
-		end if;
-		
-		case CURRENT_STATE is 
--- Player 1: Goalie -----------------------------------------------------------------
-			when "0000" => CURRENT_STATE := "0001";
-				-- draw player 1 goalie with x offset of 5
-				colour <= "111";
-				P1G_X := "00000101";
-			
-				-- draw player 1 goalie in the verticle
-				colour <= "100";
-				if (DRAW_P1G = '1') then
-					P1_GOAL := "0000100";
-				else 
-					P1_GOAL := P1_GOAL + 1;
-				end if;
-				
-				-- draw player 1 goalie for 10 pixels in the y
-				if (P1_GOAL = "0001110") then
-					DRAW_P1G <= '1';
-				else 
-					DRAW_P1G <= '0';
-				end if;
-				
-				colour <= "100";
-				x <= std_logic_vector(P1G_X(7 downto 0));
-				y <= std_logic_vector(P1_GOAL(6 downto 0));
-				plot <= '1';
--- Player 1: Goalie -----------------------------------------------------------------	
-
--- Player 1: Forward ----------------------------------------------------------------
-			when "0001" => CURRENT_STATE := "0010";
-				-- draw player 1 forward with x offset of 15
-				colour <= "111";
-				P1F_X := "00001111";
-			
-				-- draw player 1 forward in the verticle
-				colour <= "100";
-				if (DRAW_P1F = '1') then
-					P1_FORW := "0000100";
-				else 
-					P1_FORW := P1_FORW + 1;
-				end if;
-				
-				-- draw player 1 forward for 10 pixels in the y
-				if (P1_FORW = "0001110") then
-					DRAW_P1F <= '1';
-				else 
-					DRAW_P1F <= '0';
-				end if;
-				
-				colour <= "100";
-				x <= std_logic_vector(P1F_X(7 downto 0));
-				y <= std_logic_vector(P1_FORW(6 downto 0));
-				plot <= '1';
--- Player 1: Forward ----------------------------------------------------------------				
-			when others => CURRENT_STATE := "0001"; 
-			
-			end case;
-			
-		end if;
-		
-	end process;
-		
---	process(CLOCK_SLOW)
+--	-- process for the paddles:
+--	process(CLOCK_SLOW) 
 --	
---		variable Y_VAR		: unsigned(7 downto 0);
---		variable X_VAR		: unsigned(8 downto 0);
+--		variable CURRENT_STATE : unsigned(3 DOWNTO 0) := "0000";
+--	
+--		variable P1_GOAL	: unsigned(6 downto 0);
+--		variable P1G_X		: unsigned(7 downto 0);
 --		
---	begin
---					
---		-- This if block is to fill entire screen
---		if (rising_edge(CLOCK_SLOW)) then 
+--		variable P1_FORW	: unsigned(6 downto 0);
+--		variable P1F_X		: unsigned(7 downto 0);
+--		
+--	begin 
+--	
+--		if (rising_edge(CLOCK_SLOW)) then
+--		
+--		if (key(3) = '0') then
+--			CURRENT_STATE := "0000";
+--		end if;
+--		
+--		case CURRENT_STATE is 
+-- Player 1: Goalie -----------------------------------------------------------------
+--			when "0000" => CURRENT_STATE := "0001";
+--				-- draw player 1 goalie with x offset of 5
+--				colour <= "111";
+--				P1G_X := "00000101";
 --			
---			if (INITY = '1') then 
---				Y_VAR := "00000000";
---			else
---				Y_VAR := Y_VAR + 1;
---			end if;
+--				-- draw player 1 goalie in the verticle
+--				colour <= "100";
+--				if (DRAW_P1G = '1') then
+--					P1_GOAL := "0000100";
+--				else 
+--					P1_GOAL := P1_GOAL + 1;
+--				end if;
+--				
+--				-- draw player 1 goalie for 10 pixels in the y
+--				if (P1_GOAL = "0001110") then
+--					DRAW_P1G <= '1';
+--				else 
+--					DRAW_P1G <= '0';
+--				end if;
+--				
+--				colour <= "100";
+--				x <= std_logic_vector(P1G_X(7 downto 0));
+--				y <= std_logic_vector(P1_GOAL(6 downto 0));
+--				plot <= '1';
+-- Player 1: Goalie -----------------------------------------------------------------	
+--
+-- Player 1: Forward ----------------------------------------------------------------
+--			when "0001" => CURRENT_STATE := "0010";
+--				-- draw player 1 forward with x offset of 15
+--				colour <= "111";
+--				P1F_X := "00001111";
 --			
---			if (INITX = '1') then 
---				X_VAR := "000000000";
---			elsif (Y_VAR = "1110111") then
---				X_VAR := X_VAR + 1;
---			end if;
+--				-- draw player 1 forward in the verticle
+--				colour <= "100";
+--				if (DRAW_P1F = '1') then
+--					P1_FORW := "0000100";
+--				else 
+--					P1_FORW := P1_FORW + 1;
+--				end if;
+--				
+--				-- draw player 1 forward for 10 pixels in the y
+--				if (P1_FORW = "0001110") then
+--					DRAW_P1F <= '1';
+--				else 
+--					DRAW_P1F <= '0';
+--				end if;
+--				
+--				colour <= "100";
+--				x <= std_logic_vector(P1F_X(7 downto 0));
+--				y <= std_logic_vector(P1_FORW(6 downto 0));
+--				plot <= '1';
+-- Player 1: Forward ----------------------------------------------------------------				
+--			when others => CURRENT_STATE := "0001"; 
 --			
---			if (Y_VAR = "1110111") then 
---				INITY <= '1';
---			else 
---				INITY <= '0';
---			end if;
---			
---			if (X_VAR = "10011111") then
---				INITX <= '1';
---			else
---				INITX <= '0';
---			end if;
---			
---			colour <= "111";
---			x <= std_logic_vector(X_VAR(7 downto 0));
---			y <= std_logic_vector(Y_VAR(6 downto 0));
---			plot <= '1';
+--			end case;
 --			
 --		end if;
 --		
---		end process;
+--	end process;
+		
+	process(CLOCK_SLOW)
+	
+		variable Y_VAR		: unsigned(6 downto 0);
+		variable X_VAR		: unsigned(7 downto 0);
+		
+	begin
+					
+		-- This if block is to fill entire screen
+		if (falling_edge(CLOCK_SLOW)) then 
+			
+			if (INITY = '1') then 
+				Y_VAR := "0000000";
+			else
+				Y_VAR := Y_VAR + 1;
+			end if;
+			
+			if (INITX = '1') then 
+				X_VAR := "00000000";
+			end if;
+			
+			if (Y_VAR = "1110111") then 
+				INITY <= '1';
+			else 
+				INITY <= '0';
+			end if;
+			
+			if (X_VAR = "10011111" and Y_VAR = "1110111") then
+				INITX <= '1';
+			else
+				INITX <= '0';
+			end if;
+			
+			colour <= "100";
+			x <= std_logic_vector(X_VAR(7 downto 0));
+			y <= std_logic_vector(Y_VAR(6 downto 0));
+			plot <= '1';
+			
+			if (Y_VAR = "1110111") then
+			X_VAR := X_VAR + 1;
+			end if;
+			
+		end if;
+		
+		end process;
 		
 end RTL;
 
