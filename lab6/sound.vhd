e8fbd6cf26168357414352f927acc227243560cf
SMWTLM <mcw918@hotmail.com>
2014-04-02 01:29:08 -0700

    implemented structure for task3, key C and D working

--- lab6/sound.vhd
+++ lab6/sound.vhd
@@ -41,6 +41,9 @@ ARCHITECTURE Behavior OF sound IS
 	SIGNAL readdata_left, readdata_right							:STD_LOGIC_VECTOR(23 DOWNTO 0);	
 	SIGNAL reset															:STD_LOGIC;
 
+	type freq_data is array (0 to 6) of UNSIGNED(9 DOWNTO 0);--constant array of frequencies, starting from the left is C, D and so on
+	CONSTANT frequencies : freq_data := ("0001010100","0001001011","0001000011","0000111111","0000111000","0000110010","0000101101");
+
 	CONSTANT max_amp														:SIGNED(23 DOWNTO 0) := "000000001000000000000000";
 
 	type state_types is (C1 ,C2, C3);
@@ -57,10 +60,14 @@ BEGIN
 	--- rest of your code goes here
 	PROCESS(CLOCK_50,reset)
 
-		VARIABLE counter_c	:UNSIGNED(9 DOWNTO 0) := "0000000000";
-		VARIABLE switch_c		:STD_LOGIC := '0';
-		VARIABLE input_c     :SIGNED(23 DOWNTO 0) := "000000000000000000000000";
+		VARIABLE counter_c, counter_d	:UNSIGNED(9 DOWNTO 0) := "0000000000"; --counter that are going to be assigned by frequencies (the number is actually acquired by dividing frequencies by 44000 and then by 2)
+		VARIABLE switch_c, switch_d		:STD_LOGIC := '0'; --signal to switch wave (positive to negative)
+		VARIABLE input_c, input_d     :SIGNED(23 DOWNTO 0) := "000000000000000000000000"; --variable for number operation
 		VARIABLE current_state: state_types;
+		
+	--To Bojan, the following code can control C and D with switch 6 and 5. However simutaneous playback is not implemented yet.
+	--To do so you need to superimpose my the amlitude, which means you need to add them together at the end, thus some codes need to be changed
+	--GLHF!
 
 	BEGIN
 		if (reset = '1') then
@@ -74,6 +81,7 @@ BEGIN
 
 				when C2 =>
 					if(write_ready = '1') then
+					if (sw(6) = '1') then
 						if(switch_c = '0') then
 							input_c := not(max_amp) + 1;
 							writedata_left <= (STD_LOGIC_VECTOR(input_c));
@@ -85,26 +93,44 @@ BEGIN
 						end if;
 						write_s <= '1';
 						counter_c := counter_c + 1;
-						if(counter_c = "0001010100") then
+						if(counter_c = frequencies(0)) then
 							counter_c := "0000000000";
 							switch_c := not(switch_c);
 						end if;	
-						current_state := C3;
-					else
-						current_state := C1;
-					end if;	
-					
-
-				when C3 =>
-					if(write_ready = '0') then
-						write_s <= '0';
 					end if;
-						current_state := C2;
-					
+					if (sw(5) = '1') then
+						if(switch_d = '0') then
+							input_d := not(max_amp) + 1;
+							writedata_left <= (STD_LOGIC_VECTOR(input_d));
+							writedata_right <= (STD_LOGIC_VECTOR(input_d));
+						elsif(switch_d = '1') then
+							input_d := max_amp - 1;
+							writedata_left <= (STD_LOGIC_VECTOR(input_d));
+							writedata_right <= (STD_LOGIC_VECTOR(input_d));
+						end if;
+						write_s <= '1';
+						counter_d := counter_d + 1;
+						if(counter_d = frequencies(1)) then
+							counter_d := "0000000000";
+							switch_d := not(switch_d);
+						end if;	
+					end if;
+					current_state := C3;
+				else
+					current_state := C1;
+				end if;	
 
 
-			end case;
-		end if;
-	END PROCESS;
+			when C3 =>
+				if(write_ready = '0') then
+					write_s <= '0';
+				end if;
+				current_state := C2;
+
+
+
+		end case;
+	end if;
+END PROCESS;
 
 END Behavior;
